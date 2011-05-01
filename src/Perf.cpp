eac4a279132a7d20d8eff48136efa590ab5d098f
Josh Koch <jdk1337@gmail.com>
2011-05-01 17:18:24 -0400

    Added external library license information. Tweaked source license author information.

--- src/Perf.cpp
+++ src/Perf.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011 Josh Koch <jdk1337@gmail.com>
+ * Copyright (c) 2011 Josh Koch
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
